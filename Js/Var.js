8fe98b12f76d7b794efea3fc66cd48f001462ae2
andycr123 <81699558+andycr123@users.noreply.github.com>
2024-03-22 18:56:13 -0500

    eliminar

--- Js/Var.js
+++ Js/Var.js
@@ -18,5 +18,31 @@ function init(){
 
 
 function añadir(){ 
-    const clave = document.getElementById
+    const clave = document.getElementById(' clave ').value;
+    const valor = document.getElementById(' valor ').value;
+
+
+    if(clave !='') {
+      
+        if(localStorage.getItem(clave)) {
+            document.getElementById(' mensaje ').innerHTML = ' <p> Se edito el registro </p> ';
+        } else {
+            document.getElementById(' mensaje ').innerHTML = ' <p> Se añadio el registro </p> ';
+        }
+
+        localStorage.setItem(clave, valor);
+    } else {
+        document.getElementById(' mensaje ').innerHTML = '<p> La clave tiene que tener un valor </p>';
+    }
+}
+
+function eliminar (){
+    const clave = document.getElementById(' clave ').value;
+
+    if(localStorage.getItem(' clave ')) { 
+        document.getElementById(' mensaje ').innerHTML = ' <p> Se elimino el registro</p> ';
+        localStorage.removeItem(clave);
+    } else {
+        document.getElementById(' mensaje ').innerHTML = ' <> La clave no existe </>  '
+    }
 }
